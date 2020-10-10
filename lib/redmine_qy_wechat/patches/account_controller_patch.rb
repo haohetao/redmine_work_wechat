5bbae9e7e94354ccebcbf3e305d51386771d5b26
hetao <haohetao@gmail.com>
2020-10-10 13:43:28 +0800

    fix gingtalk apiUrl

--- lib/redmine_qy_wechat/patches/account_controller_patch.rb
+++ lib/redmine_qy_wechat/patches/account_controller_patch.rb
@@ -9,14 +9,14 @@ module RedmineQyWechat
         base.class_eval do
           unloadable # Send unloadable so it will not be unloaded in development
           # defind a globle var for backurl
-          # 适配4.0 
+          # 适配4.0
           alias_method :login_without_login_dingtalk, :login
           alias_method :login, :login_with_login_dingtalk
           alias_method :successful_authentication_without_login_dingtalk, :successful_authentication
           alias_method :successful_authentication, :successful_authentication_with_login_dingtalk
         end
       end
-    
+
       module InstanceMethods
         def login_with_login_dingtalk
           logger.info("redmine_work_wechat login ------------------------------")
@@ -24,19 +24,24 @@ module RedmineQyWechat
           logger.info("auth_code:#{auth_code}")
           if auth_code.blank?
             code = params[:code]
-            state = params[:state]            
+            state = params[:state]
             # 如果是钉钉登录回调
             if state == "STATE"
               appid = Setting["plugin_redmine_work_wechat"]["dingtalk_login_appid"]
               appsecret = Setting["plugin_redmine_work_wechat"]["dingtalk_login_appsecret"]
               redirect_url = Setting["plugin_redmine_work_wechat"]["dingtalk_login_redirect"]
+              apiUrl = Setting["plugin_redmine_work_wechat"]["dingtalk_api_url"]
+              if apiUrl.blank?
+                apiUrl = "https://oapi.dingtalk.com"
+              end
               # logger.info(appid)
               # logger.info(appsecret)
               # logger.info(redirect_url)
+              logger.info(apiUrl)
               if (appid.blank? || appsecret.blank? || redirect_url.blank?)
                 return
               end
-            
+
               begin
               timestamp = (Time.now.to_f * 1000).to_i
               logger.info("timestamp:#{timestamp}")
@@ -57,15 +62,15 @@ module RedmineQyWechat
                  "tmp_auth_code": code
               }.to_json
 
-              url = URI.parse("https://oapi.dingtalk.com/sns/getuserinfo_bycode?accessKey=#{appid}&timestamp=#{timestamp}&signature=#{signature}")
+              url = URI.parse(apiUrl + "/sns/getuserinfo_bycode?accessKey=#{appid}&timestamp=#{timestamp}&signature=#{signature}")
               http = Net::HTTP.new(url.host,url.port)
               http.use_ssl = true
-              
+
               # logger.info("url.request_uri")
               # logger.info(url.request_uri)
               #req = Net::HTTP::Post.new(url.path, initheader = {'Content-Type' =>'application/json'})
               req = Net::HTTP::Post.new(url.request_uri, 'Content-Type' => 'application/json')
-                  
+
               req.body = data
               res = http.request(req)
               logger.info("response")
@@ -73,7 +78,7 @@ module RedmineQyWechat
               logger.info(JSON.parse(res.body)["errmsg"])
               logger.info(JSON.parse(res.body)["user_info"])
               logger.info(JSON.parse(res.body)["user_info"]["unionid"])
-            
+
               # 获得用户id
               $dingid = JSON.parse(res.body)["user_info"]["dingId"]
               logger.info("dingid:#{$dingid}")
@@ -83,10 +88,10 @@ module RedmineQyWechat
                 flash[:notice] = l(:flash_dingtalk_bind)
                 return
               end
-              
+
               logger.info("查询是否绑定dingId:#{$dingid}")
               user = User.find_by dingtalk_dingid: $dingid unless $dingid.blank?
-              
+
               logger.info("是否存在该用户:#{!user.blank?}")
               unless user.blank?
                 if user.active?
@@ -106,6 +111,11 @@ module RedmineQyWechat
             appkey = Setting["plugin_redmine_work_wechat"]["dingtalk_app_key"]
             #填写确认并应用的应用Secret
             appsecret = Setting["plugin_redmine_work_wechat"]["dingtalk_app_secret"]
+            apiUrl = Setting["plugin_redmine_work_wechat"]["dingtalk_api_url"]
+            if apiUrl.blank?
+              apiUrl = "https://oapi.dingtalk.com"
+            end
+            logger.info("apiUrl:#{apiUrl}")
             logger.info("appkey:#{appkey}")
             logger.info("appsecret:#{appsecret}")
             if (appkey.blank? || appsecret.blank?)
@@ -113,34 +123,34 @@ module RedmineQyWechat
               redirect_to home_url
               return
             end
-            
+
             begin
-              uri = URI.parse("https://oapi.dingtalk.com/gettoken?appkey=#{appkey}&appsecret=#{appsecret}")
+              uri = URI.parse(apiUrl + "/gettoken?appkey=#{appkey}&appsecret=#{appsecret}")
               http = Net::HTTP.new(uri.host, uri.port)
               http.use_ssl = true
-              request = Net::HTTP::Get.new(uri.request_uri)  
+              request = Net::HTTP::Get.new(uri.request_uri)
               response = http.request(request)
-          
+
               # 获得token
               token = JSON.parse(response.body)["access_token"]
               logger.info("token:#{token}")
-              
-              uri = URI.parse("https://oapi.dingtalk.com/user/getuserinfo?access_token=#{token}&code=#{auth_code}")
+
+              uri = URI.parse(apiUrl + "/user/getuserinfo?access_token=#{token}&code=#{auth_code}")
               http = Net::HTTP.new(uri.host, uri.port)
               http.use_ssl = true
-              request = Net::HTTP::Get.new(uri.request_uri)  
+              request = Net::HTTP::Get.new(uri.request_uri)
               response = http.request(request)
-          
+
               # 获得errcode
               err_code = JSON.parse(response.body)["errcode"]
-              logger.info("err_code:#{err_code}")          
+              logger.info("err_code:#{err_code}")
               if err_code != 0
                 flash[:error] = l(:flash_dingtalk_autologin_error)
                 redirect_to home_url
                 return
               end
-              
-              dingtalk_user_id = JSON.parse(response.body)["userid"]          
+
+              dingtalk_user_id = JSON.parse(response.body)["userid"]
               logger.info("dingtalk_user_id:#{dingtalk_user_id}")
             rescue
               flash[:error] = l(:flash_dingtalk_autologin_error)
@@ -163,7 +173,7 @@ module RedmineQyWechat
           login_without_login_dingtalk
         end
       end
-      
+
       def successful_authentication_with_login_dingtalk(user)
         if !$dingid.blank?
           # 更新当前的dingid
--- lib/redmine_qy_wechat/patches/corp_wechats_journals_patch.rb
+++ lib/redmine_qy_wechat/patches/corp_wechats_journals_patch.rb
@@ -7,27 +7,27 @@ module CorpWechatsJournalsPatch
   included do
     after_create :send_messages_after_create_journal
   end
-  
-  # 用企业微信发送 
+
+  # 用企业微信发送
   def send_by_wechat(send_people_wx)
        #填写确认并应用的企业ID
       corpid = Setting["plugin_redmine_work_wechat"]["wechat_corp_id"]
       #填写确认并应用的应用Secret
       corpsecret = Setting["plugin_redmine_work_wechat"]["wechat_app_secret"]
-      
+
       app_id = Setting["plugin_redmine_work_wechat"]["wechat_app_id"]
-            
+
       if corpid.blank? || corpsecret.blank? || app_id.blank?
         return
       end
       @group_client = QyWechatApi::Client.new(corpid, corpsecret)
       # 为了确保用户输入的corpid, corpsecret是准确的，请务必执行：
-        
+
       if @group_client.blank?
         return
       end
-  
-      
+
+
       # 改成异常捕捉，避免is_valid?方法本身的出错
       begin
         if @group_client.is_valid?
@@ -36,15 +36,15 @@ module CorpWechatsJournalsPatch
         #group_client = QyWechatApi::Client.new(corpid, corpsecret, options)
         #issue
         #填写确认并应用的应用AgentId
-  
+
           @group_client.message.send_text(send_people_wx, "", "", app_id,
           "#{l(:msg_focus)} <a href=\'" + Setting.protocol + "://" + Setting.host_name + "/issues/#{@issue.id}\'>#{@issue.tracker} ##{@issue.id}: #{@issue.subject}</a> #{l(:msg_by)} <a href=\'javascript:void(0);\'>#{@issue.journals.last.user}</a> #{l(:msg_updated)}")
         end
       rescue
       end
   end
-      
-  # 用钉钉发送 
+
+  # 用钉钉发送
   def send_by_dingtalk(send_people_dd)
     #填写确认并应用的企业ID
     corpid = Setting["plugin_redmine_work_wechat"]["dingtalk_corp_id"]
@@ -54,7 +54,11 @@ module CorpWechatsJournalsPatch
     appkey = Setting["plugin_redmine_work_wechat"]["dingtalk_app_key"]
     #填写确认并应用的应用Secret
     appsecret = Setting["plugin_redmine_work_wechat"]["dingtalk_app_secret"]
-            
+    apiUrl = Setting["plugin_redmine_work_wechat"]["dingtalk_api_url"]
+    if apiUrl.blank?
+      apiUrl = "https://oapi.dingtalk.com"
+    end
+    logger.info(apiUrl)
     logger.info(corpid)
     logger.info(appkey)
     logger.info(appsecret)
@@ -63,27 +67,27 @@ module CorpWechatsJournalsPatch
       return
     end
     # uri = URI.parse("https://oapi.dingtalk.com/gettoken?corpid=#{corpid}&corpsecret=#{corpsecret}")
-    uri = URI.parse("https://oapi.dingtalk.com/gettoken?appkey=#{appkey}&appsecret=#{appsecret}")
+    uri = URI.parse(apiUrl + "/gettoken?appkey=#{appkey}&appsecret=#{appsecret}")
     # 改成异常捕捉，避免is_valid?方法本身的出错
     begin
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       request = Net::HTTP::Get.new(uri.request_uri)
-            
+
       response = http.request(request)
-          
+
       # 获得token
       token = JSON.parse(response.body)["access_token"]
       logger.info(JSON.parse(response.body)["errcode"])
       logger.info(JSON.parse(response.body)["errmsg"])
       logger.info("token:#{token}")
-    
+
       # issue_url = issue_url(@issue)
       issue_url =  Setting.protocol + "://" + Setting.host_name + "/issues/#{@issue.id}"
       issue_title = @issue.project.name
-      
+
       issue_text = "#{@issue.tracker} ##{@issue.id}: #{@issue.subject} #{@issue.status} #{@issue.done_ratio}% #{l(:msg_by)} #{@issue.author} #{l(:msg_created)}"
-    
+
       data = {
         msg: {
           msgtype: "link",
@@ -95,20 +99,20 @@ module CorpWechatsJournalsPatch
           }
         }
       }.to_json
-        
+
       logger.info("start request")
       logger.info(data)
-      url = URI.parse("https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2?access_token=#{token}&agent_id=#{appid}&userid_list=#{send_people_dd}") 
-      # url = URI.parse("https://oapi.dingtalk.com/chat/send?access_token=#{token}")  
-      # url = URI.parse("https://oapi.dingtalk.com/chat/send?access_token=#{token}&chatid=chatd58978ab82b4e2598b4c5540a1241d41")  
+      url = URI.parse(apiUrl + "/topapi/message/corpconversation/asyncsend_v2?access_token=#{token}&agent_id=#{appid}&userid_list=#{send_people_dd}")
+      # url = URI.parse("https://oapi.dingtalk.com/chat/send?access_token=#{token}")
+      # url = URI.parse("https://oapi.dingtalk.com/chat/send?access_token=#{token}&chatid=chatd58978ab82b4e2598b4c5540a1241d41")
       http = Net::HTTP.new(url.host,url.port)
       http.use_ssl = true
-      
+
       logger.info("url.request_uri")
       logger.info(url.request_uri)
       #req = Net::HTTP::Post.new(url.path, initheader = {'Content-Type' =>'application/json'})
       req = Net::HTTP::Post.new(url.request_uri, 'Content-Type' => 'application/json')
-      
+
       req.body = data
       # req.body = dingtalk_account_number
       res = http.request(req)
@@ -131,17 +135,17 @@ module CorpWechatsJournalsPatch
     to_users = notified_users
     cc_users = notified_watchers - to_users
     notify_users = to_users + cc_users
-    
-    
+
+
     issue = journalized
     @issue = issue
-    
+
     if notify?
       # 需要接受微信和钉钉的用户ID集合
       send_people_wx = ""
       send_people_dd = ""
       # 用@issue自带的方法获取需要通知的用户列表
-            
+
       notify_users.each do |user|
         unless user.corp_wechat_account_number.blank?
           send_people_wx.concat(user.corp_wechat_account_number).concat("|")
@@ -150,7 +154,7 @@ module CorpWechatsJournalsPatch
           send_people_dd.concat(user.dingtalk_account_number).concat(",")
         end
       end
-      
+
 
       # # 作者
       # unless @issue.author_id.nil?
@@ -158,7 +162,7 @@ module CorpWechatsJournalsPatch
       #     send_people_wx.concat(User.where(:id => @issue.author_id).first.corp_wechat_account_number).concat("|")
       #   end
       # end
-      
+
       # # 指派者
       # unless @issue.assigned_to_id.nil?
       #   unless User.where(:id => @issue.assigned_to_id).first.corp_wechat_account_number.blank?
@@ -172,11 +176,11 @@ module CorpWechatsJournalsPatch
       #     send_people_wx.concat(User.where(:id => information.id).first.corp_wechat_account_number).concat("|")
       #   end
       # end
-      
+
       if !send_people_wx.blank?
         send_by_wechat send_people_wx
       end
-      
+
       # 以下是钉钉的处理
       # 作者
       # unless @issue.author_id.nil?
@@ -184,7 +188,7 @@ module CorpWechatsJournalsPatch
       #     send_people_dd.concat(User.where(:id => @issue.author_id).first.dingtalk_account_number).concat("|")
       #   end
       # end
-      
+
       # # 指派者
       # unless @issue.assigned_to_id.nil?
       #   unless User.where(:id => @issue.assigned_to_id).first.dingtalk_account_number.blank?
@@ -198,7 +202,7 @@ module CorpWechatsJournalsPatch
       #     send_people_dd.concat(User.where(:id => information.id).first.dingtalk_account_number).concat("|")
       #   end
       # end
-      
+
       if !send_people_dd.blank?
         send_by_dingtalk send_people_dd
       end
